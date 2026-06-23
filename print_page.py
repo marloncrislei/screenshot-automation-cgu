cd116dad582b0831c9df9e6c09050eca09d18606
marlon.silva <marloncsilva@gmail.com>
2026-06-23 07:08:13 -0300

    Adicionar opções de Chrome para container

--- print_page.py
+++ print_page.py
@@ -56,12 +56,18 @@ def criar_driver():
     """Cria driver Chrome com opções para servidor"""
     chrome_options = Options()
     
+    # Modo headless (sem interface gráfica)
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--window-size=1920,1080")
+    chrome_options.add_argument("--disable-web-resources")
+    chrome_options.add_argument("--disable-extensions")
+    chrome_options.add_argument("--disable-plugins")
+    chrome_options.add_argument("--disable-images")
     chrome_options.add_argument("--start-maximized")
+    chrome_options.add_argument("--remote-debugging-port=9222")
     
     if not SHOW_BROWSER:
         chrome_options.add_experimental_option("detach", True)
